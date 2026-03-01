aab3a1a47b7448ea258752a0ffcd1baee99a23d1
zcm <lsj88lyr@gmail.com>
2026-03-01 12:32:06 +0800

    jsj

--- mem_cleaner_ebpf/src/main.rs
+++ mem_cleaner_ebpf/src/main.rs
@@ -1,3 +1,6 @@
+#![no_std]
+#![no_main]
+
 use aya_ebpf::{
     macros::{map, tracepoint},
     maps::PerfEventArray,
@@ -17,16 +20,10 @@ pub fn sched_process_exec(ctx: TracePointContext) -> u32 {
 }
 
 fn try_sched_process_exec(ctx: TracePointContext) -> Result<u32, u32> {
-    // 获取当前 pid
     let pid_tgid = aya_ebpf::helpers::bpf_get_current_pid_tgid();
     let pid = (pid_tgid >> 32) as u32;
 
-    // sched_process_exec tracepoint 的 comm 在偏移 0 处
-    // 这里简单读取 uid（可选），也可以只传 pid
-    let uid = unsafe { ctx.read_at::<u32>(0).unwrap_or(0) };
-
-    let event = ProcessEvent { pid, uid };
-
+    let event = ProcessEvent { pid, uid: 0 };
     EVENTS.output(&ctx, &event, 0);
 
     Ok(0)
