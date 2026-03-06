b58473ca4c781925dd29fcd914402ed91afffe34
zcm <lsj88lyr@gmail.com>
2026-03-06 22:50:23 +0800

    qqqq

--- mem_cleaner_ebpf/src/main.rs
+++ mem_cleaner_ebpf/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use aya_ebpf::{
+    helpers::bpf_get_current_comm,
     macros::{map, tracepoint},
     maps::PerfEventArray,
     programs::TracePointContext,
@@ -11,29 +12,37 @@ use mem_cleaner_common::ProcessEvent;
 #[map]
 static EVENTS: PerfEventArray<ProcessEvent> = PerfEventArray::new(0);
 
+const TASK_COMM_LEN: usize = 16;
+
 #[tracepoint]
 pub fn sched_process_fork(ctx: TracePointContext) -> u32 {
-    // 1. 获取触发 Fork 的父进程 UID
-    let uid = (aya_ebpf::helpers::bpf_get_current_uid_gid() & 0xFFFFFFFF) as u32;
+    // 1. 获取当前进程名
+    let mut comm = [0u8; TASK_COMM_LEN];
+    let ret = unsafe { bpf_get_current_comm(comm.as_mut_ptr(), TASK_COMM_LEN as u32) };
+    if ret != 0 {
+        return 0;
+    }
 
-    // 2. 终极过滤：只允许 Root (UID 0) 触发的事件通过！
-    // Zygote 孵化 App 时，身份是 UID 0。
-    // App 运行后自己创建几百个下载线程时，身份是 10xxx，直接在内核被无情抛弃，零开销！
-    if uid == 0 {
-        // 读取 Linux 64位 sched_process_fork 结构体中的 child_pid (偏移量44)
-        let child_pid_offset = 44;
-        let child_pid: u32 = unsafe {
-            match ctx.read_at(child_pid_offset) {
-                Ok(val) => val,
-                Err(_) => return 0,
-            }
-        };
+    // 2. 过滤，只允许 zygote/zygote64
+    if !(comm.starts_with(b"zygote") || comm.starts_with(b"zygote64")) {
+        return 0;
+    }
 
-        // 3. 将极其纯净的 PID 发给用户态
-        let event = ProcessEvent {
-            pid: child_pid,
-            _padding: 0,
-        };
+    // 3. 硬编码偏移量读取 child_pid
+    let child_pid_offset = 44;
+    let child_pid: u32 = unsafe {
+        match ctx.read_at(child_pid_offset) {
+            Ok(val) => val,
+            Err(_) => return 0,
+        }
+    };
+
+    // 4. 输出到用户态
+    let event = ProcessEvent {
+        pid: child_pid,
+        _padding: 0,
+    };
+    unsafe {
         EVENTS.output(&ctx, &event, 0);
     }
 
