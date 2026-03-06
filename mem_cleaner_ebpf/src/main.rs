f0ca011d4397d7c5aaf1b5d9b307e52a301e18af
zcm <lsj88lyr@gmail.com>
2026-03-06 21:46:11 +0800

    好!

--- mem_cleaner_ebpf/src/main.rs
+++ mem_cleaner_ebpf/src/main.rs
@@ -11,33 +11,32 @@ use mem_cleaner_common::ProcessEvent;
 #[map]
 static EVENTS: PerfEventArray<ProcessEvent> = PerfEventArray::new(0);
 
+#[repr(C)]
+struct ForkEvent {
+    child_pid: u32,
+}
+
 #[tracepoint]
 pub fn sched_process_fork(ctx: TracePointContext) -> u32 {
-    // Linux 64-bit (ARM64) sched_process_fork 布局通常如下：
-    // common_fields (8 bytes)
-    // parent_comm   (16 bytes)
-    // parent_pid    (4 bytes)
-    // child_comm    (16 bytes)
-    // child_pid     (4 bytes) <--- 我们要抓这个
-    //
-    // Offset = 8 + 16 + 4 + 16 = 44 bytes
-    let child_pid_offset = 44;
+    // 1. 获取触发 fork 的父进程 UID
+    let uid = (aya_ebpf::helpers::bpf_get_current_uid_gid() & 0xFFFFFFFF) as u32;
 
-    // 读取子进程 PID
-    let child_pid: u32 = unsafe {
-        match ctx.read_at(child_pid_offset) {
-            Ok(val) => val,
-            Err(_) => return 0,
-        }
-    };
+    // 2. 只拦截 Root (UID = 0) 触发的事件
+    if uid == 0 {
+        let ev: ForkEvent = unsafe {
+            match ctx.read_at(44) {
+                Ok(val) => val,
+                Err(_) => return 0,
+            }
+        };
 
-    // 发送事件给用户态
-    let event = ProcessEvent {
-        pid: child_pid,
-        _padding: 0,
-    };
-
-    EVENTS.output(&ctx, &event, 0);
+        // 发送给用户态
+        let event = ProcessEvent {
+            pid: ev.child_pid,
+            _padding: 0,
+        };
+        EVENTS.output(&ctx, &event, 0);
+    }
 
     0
 }
